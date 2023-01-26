37dddbf3e0f4715c213fade32d9c9ae87abd5593
Наталья Сорокина <natalya_40ina@mail.ru>
2023-01-26 21:31:58 +0300

    tests

--- api_yamdb/core/management/commands/loadyamdbdata.py
+++ api_yamdb/core/management/commands/loadyamdbdata.py
@@ -1,7 +1,7 @@
-from django.core.management.base import BaseCommand
 from csv import DictReader
 
-from reviews.models import Category, Comment, Genre, TitleGenre, Title, Review
+from django.core.management.base import BaseCommand
+from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
 from users.models import User
 
 
